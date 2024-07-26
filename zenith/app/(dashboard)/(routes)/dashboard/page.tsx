217ba172dddbc0d36f580987f31e79273ad16bb0
Koshik <koshik.gurbani@aceturtle.com>
2024-07-26 20:28:31 +0530

    reformed the folder structure and made two routes for Dashboard and landing page

---
+++ zenith/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,16 @@
+/*
+import { Button } from "@/components/ui/button";
+
+ export default function DashboardPage() {
+  return (
+    <Button variant = "destructive">Click Me</Button>
+  );
+} */
+
+const DashboardPage = () => {
+    return (
+        <div>Dashboard Page (Protected)</div>
+    )
+}
+
+export default DashboardPage
